dec2267e9bc5c0fbbdefe6db1fd6a0a44c952dc4
A. Edson Maribone <allan.edson@maribone.com.br>
2025-12-30 01:09:40 -0300

    Update complementa.js

--- complementa.js
+++ complementa.js
@@ -1,21 +1,14 @@
 $(document).ready(function () {
 
-    // MAPA
     var map = L.map('map', {
         scrollWheelZoom: false
     }).setView([-23.4866, -46.3487], 16);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19
+        maxZoom: 19,
+        className: 'blue-map'
     }).addTo(map);
 
-   
-    
-
-    // AJUSTA ZOOM AO TERRITÓRIO REAL
-    map.fitBounds(itaquaLayer.getBounds());
-
-    // MARCADOR CENTRAL
     L.marker([-23.4866, -46.3487])
         .addTo(map)
         .bindPopup('Itaquaquecetuba — Território Soberano');
