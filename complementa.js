fbaa1437e491f1ee2da1074d25f8035f95ad5d2b
A. Edson Maribone <allan.edson@maribone.com.br>
2026-01-03 16:53:42 -0300

    Update complementa.js

--- complementa.js
+++ complementa.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     }).setView([-23.4866, -46.3487], 16);
 
     // A OPÇÃO 3: O CAMINHO DA PAZ E DA NITIDEZ
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
     attribution: '© OpenStreetMap contributors, © CARTO',
     subdomains: 'abcd',
     maxZoom: 19
