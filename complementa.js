f145e6183ad6b32c4c240c110e92ab4b3e57fc9f
A. Edson Maribone <allan.edson@maribone.com.br>
2026-01-03 21:13:09 -0300

    Update complementa.js

--- complementa.js
+++ complementa.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         scrollWheelZoom: false
     }).setView([-23.4866, -46.3487], 16);
 
-   // O ESTILO TÁTICO E VIBRANTE DE VOSSA MAJESTADE
-L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=eb43632506e746538c642e0f498c56e3', {
-	attribution: '&copy; Thunderforest, &copy; OpenStreetMap contributors',
-	maxZoom: 19
+  // O MAPA QUE NUNCA FALHA — BASE SÓLIDA
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    className: 'mapa-real-preto' // A etiqueta de Vossa Majestade
 }).addTo(map);
 
    
