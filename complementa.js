c7c122989a130983ac3629b7030097cd576530d1
A. Edson Maribone <allan.edson@maribone.com.br>
2026-01-03 21:11:05 -0300

    Update complementa.js

--- complementa.js
+++ complementa.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         scrollWheelZoom: false
     }).setView([-23.4866, -46.3487], 16);
 
-   // O MAPA LIVRE E SOBERANO (ESTILO ESCURO COM NOMES)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+   // O ESTILO TÁTICO E VIBRANTE DE VOSSA MAJESTADE
+L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=eb43632506e746538c642e0f498c56e3', {
+	attribution: '&copy; Thunderforest, &copy; OpenStreetMap contributors',
+	maxZoom: 19
 }).addTo(map);
 
    
