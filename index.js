858346fcc5ec3c23d59fe6a180f558362dc53c68
A. Edson Maribone <allan.edson@maribone.com.br>
2026-01-29 17:06:30 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+ $(document).ready(function () {
+
+
+// MAPA
+
+var map = L.map('map', {
+
+scrollWheelZoom: false
+
+}).setView([-23.4866, -46.3487], 16);
+
+
+// O MAPA QUE NUNCA FALHA — BASE SÓLIDA
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+maxZoom: 19,
+
+className: 'mapa-real-preto' // A etiqueta de Vossa Majestade
+
+}).addTo(map);
+
+
+
+// AJUSTA ZOOM AO TERRITÓRIO REAL
+
+map.fitBounds(itaquaLayer.getBounds());
+
+
+// MARCADOR CENTRAL
+
+L.marker([-23.4866, -46.3487])
+
+.addTo(map)
+
+.bindPopup('Itaquaquecetuba — Território Soberano');
+
+
+}); 
