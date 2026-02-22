61727685ce9207758a663bf5ce82acb403acca94
A. Edson Maribone <allan.edson@maribone.com.br>
2026-02-21 23:40:27 -0300

    Update motorista.js

--- motorista.js
+++ motorista.js
@@ -24,3 +24,37 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 });
 __________________________________________________________________________________________________________________________________________________________________
+
+
+function sendLocationToSheet(position) {
+    const endpoint = "https://script.google.com/macros/s/AKfycbwxiNFyhwQh_B0e2efWkC6k8hWiJuzpJOPrcu2bv1qbXEVmGc6cakSPbIY90_0sR1EnAA/exec";
+    const data = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        accuracy: position.coords.accuracy,
+        status: navigator.onLine ? "online" : "offline"
+    };
+
+    fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(res => console.log("Enviado:", res))
+    .catch(err => console.error("Erro ao enviar:", err));
+}
+
+function captureGPS() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            sendLocationToSheet,
+            err => console.error("Erro de GPS:", err),
+            { enableHighAccuracy: true, maximumAge: 0, timeout: 10000 }
+        );
+    } else {
+        console.error("GPS não suportado");
+    }
+}
+
+// Opcional: capturar periodicamente a posição
+setInterval(captureGPS, 100000); // a cada 100s
