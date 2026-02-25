13da950e5a648a4925b34b276c1d3fd45042b69f
A. Edson Maribone <allan.edson@maribone.com.br>
2026-02-25 01:23:18 -0300

    Atualizar o motorista.js

--- motorista.js
+++ motorista.js
@@ -1,32 +1,20 @@
 $(document).ready(function () {
 
-    // MAPA
     var map = L.map('map', {
         scrollWheelZoom: false
     }).setView([-23.4866, -46.3487], 16);
 
-    // BASE
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        className: 'mapa-real-preto'
+        maxZoom: 19
     }).addTo(map);
 
-    // MARCADOR CENTRAL
-    L.marker([-23.4866, -46.3487])
-        .addTo(map)
-        .bindPopup('Itaquaquecetuba — Território Soberano')
-        .openPopup();
-
-    // 🔥 ISSO AQUI FAZ PEDIR PERMISSÃO DE GPS
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            function(position) {
-                console.log("Permissão concedida");
-            },
-            function(error) {
-                console.log("Permissão negada ou erro");
-            }
-        );
-    }
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            console.log("Permissão concedida");
+        },
+        function(error) {
+            console.log("Permissão negada ou erro");
+        }
+    );
 
 });
