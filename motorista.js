deb08b0a9b7bd46f118c0b920516a79b08e6283a
A. Edson Maribone <allan.edson@maribone.com.br>
2026-02-25 02:31:17 -0300

    Update motorista.js

--- motorista.js
+++ motorista.js
@@ -9,25 +9,36 @@ $(document).ready(function () {
     }).addTo(map);
 
     navigator.geolocation.getCurrentPosition(
-    function(position) {
+        function(position) {
 
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
 
-        L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup("SEU LOCAL")
-            .openPopup();
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup("SEU LOCAL")
+                .openPopup();
 
-        map.setView([lat, lng], 17);
+            map.setView([lat, lng], 17);
 
-        fetch("https://script.google.com/macros/s/AKfycbz6bm4rqeA6_88ztbBVwr_JnQFBmVdsA8Gz9p1pK9heomd9-HFge8Ny6VPF30I5H57LQQ/exec", {
-            method: "POST",
-            body: JSON.stringify({
-                latitude: lat,
-                longitude: lng
+            fetch("https://script.google.com/macros/s/AKfycbz6bm4rqeA6_88ztbBVwr_JnQFBmVdsA8Gz9p1pK9heomd9-HFge8Ny6VPF30I5H57LQQ/exec", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    latitude: lat,
+                    longitude: lng
+                })
             })
-        });
+            .then(response => response.json())
+            .then(data => console.log("Enviado com sucesso:", data))
+            .catch(error => console.error("Erro ao enviar:", error));
 
-    }
-);
+        },
+        function(error) {
+            console.error("Erro na geolocalização:", error);
+        }
+    );
+
+});
